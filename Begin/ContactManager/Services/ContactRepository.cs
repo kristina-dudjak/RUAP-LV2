2c5ddabf8fdb8063530bb757631d48df84c179d0
kristina <dudjakkristina@gmail.com>
2021-11-19 23:08:51 +0100

    Adding Data-Persistence Features to the Contact Repository Implementation

--- Begin/ContactManager/Services/ContactRepository.cs
+++ Begin/ContactManager/Services/ContactRepository.cs
@@ -3,26 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ContactManager.Models;
+using Microsoft.AspNetCore.Http;
+using System.Runtime.Caching;
 
 namespace ContactManager.Services
 {
     public class ContactRepository
     {
+        private const string CacheKey = "ContactStore";
+
+        public ContactRepository()
+        {
+            if(MemoryCache.Default[CacheKey] == null)
+            {
+                var contacts = new Contact[]
+            {
+                new Contact
+                {
+                    Id = 1, Name = "Glenn Block"
+                },
+                new Contact
+                {
+                    Id = 2, Name = "Dan Roth"
+                }
+            };
+                MemoryCache.Default[CacheKey] = contacts;
+            }
+        }
         public Contact[] GetAllContacts()
         {
+            if (MemoryCache.Default[CacheKey] != null)
+            {
+                return (Contact[])MemoryCache.Default[CacheKey];
+            }
+
             return new Contact[]
             {
-            new Contact
-            {
-                Id = 1,
-                Name = "Glenn Block"
-            },
-            new Contact
-            {
-                Id = 2,
-                Name = "Dan Roth"
-            }
+                new Contact
+                {
+                    Id = 0,
+                    Name = "Placeholder"
+                }
             };
+        }
+
+        public bool SaveContact(Contact contact)
+        {
+            try
+            {
+                var currentData = ((Contact[])MemoryCache.Default[CacheKey]).ToList();
+                currentData.Add(contact);
+                MemoryCache.Default[CacheKey] = currentData.ToArray();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
         }
     }
 }
